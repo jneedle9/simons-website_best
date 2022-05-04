c0201a7f9388170fa5e22baaebde27e91fa17b4b
Josh N <jneedle9@gmail.com>
2022-05-04 17:35:27 -0400

    add redirect on /studio to edit content

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/studio',
+        destination: 'https://simonswebsite.sanity.studio/desk/',
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
 }
+
+
+
+
+
 
 module.exports = nextConfig
